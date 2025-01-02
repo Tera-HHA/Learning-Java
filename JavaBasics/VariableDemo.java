bb8d841cb812f55784584499657c0b1f61827c7a
Tera-HHA <phochit581@gmail.com>
2025-01-02 12:01:59 +0630

    Added VariableDemo.java

---
+++ JavaBasics/VariableDemo.java
@@ -0,0 +1,35 @@
+class VariableDemo{
+	String name;
+	static int age;
+
+	public static void main(String[] args) {
+		//Global Variables
+		//name = "Aung Aung";
+		//age = 40;
+		//System.out.println("Hello,");
+		//System.out.println("Age :"+age);
+
+		// Human human = new Human();
+		// human.staticValue = "static value";
+		// human.instanceValue = "instance value";
+		// System.out.println(human.staticValue);
+		// System.out.println(human.instanceValue);
+
+		// Human human1 = new Human();
+		// System.out.println(human1.staticValue);
+		// System.out.println(human1.instanceValue);
+
+		//local variable
+		String message = "Hello local variables";
+		System.out.println("messages :"+message);
+
+
+	}
+	
+	  // void addName(){
+	  // 	name = "Aung Aung";
+	// 	message = "Hi";
+	  // }
+ 
+	
+}
